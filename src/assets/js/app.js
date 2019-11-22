bff8f82e17be90cb3c8d52bc8b1fe57668d38b0e
RazorMeister <battnik90@gmail.com>
2019-11-22 19:25:16 +0100

    Add require.js

---
+++ src/assets/js/app.js
@@ -0,0 +1,13 @@
+requirejs.config({
+    baseUrl: 'assets/js/lib',
+	paths: {
+		'jquery': 'jquery.min',
+		'bootstrap': 'bootstrap.bundle.min',
+		'browser': 'browser.min',
+		'breakpoints': 'breakpoints.min'
+	},
+});
+
+require(["jquery", "bootstrap", "browser", "breakpoints"], () => {
+	require(["assets/js/docs.js", "assets/js/util.js", "assets/js/main.js", "assets/js/rangy.js", "assets/js/backToTop.js", "assets/js/input.js", "assets/js/anim.js"]);
+});
