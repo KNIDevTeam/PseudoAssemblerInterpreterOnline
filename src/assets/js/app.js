29bc5c80886e3d212c58cbd9962bca7bd1b358c8
RazorMeister <battnik90@gmail.com>
2019-11-22 22:19:56 +0100

    Add pace.js (preloader)

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,6 +1,7 @@
 requirejs.config({
     baseUrl: 'assets/js/lib',
 	paths: {
+		'pace': 'pace.min',
 		'jquery': 'jquery.min',
 		'bootstrap': 'bootstrap.bundle.min',
 		'browser': 'browser.min',
@@ -8,6 +9,7 @@ requirejs.config({
 	},
 });
 
-require(["jquery", "bootstrap", "browser", "breakpoints"], () => {
+require(["pace", "jquery", "bootstrap", "browser", "breakpoints"], (pace) => {
+	pace.start();
 	require(["assets/js/language.js", "assets/js/util.js", "assets/js/main.js", "assets/js/rangy.js", "assets/js/backToTop.js", "assets/js/input.js", "assets/js/anim.js"]);
 });
