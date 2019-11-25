84f6a6ea04e6196e11e25d2b24333e2ec1728048
RazorMeister <battnik90@gmail.com>
2019-11-25 19:20:23 +0100

    Test cookie

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -10,6 +10,13 @@
 }*/
 	
 	
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  var expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+	
 window.cookieconsent.initialise({
   "palette": {
     "popup": {
@@ -28,6 +35,8 @@ window.cookieconsent.initialise({
     "href": "https://wszystkoociasteczkach.pl/polityka-cookies/"
   }
 });
+
+setCookie("test", 420, 420);
 
 (function($) {
 
