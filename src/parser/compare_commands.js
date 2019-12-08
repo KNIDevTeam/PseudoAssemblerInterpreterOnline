94b88328221ab4a099e03dfb9fa8a19bc317f724
Wojciech Drochomirecki <witek.droch@gmail.com>
2019-12-08 14:57:27 +0100

    source fix in compare memory

--- src/parser/compare_commands.js
+++ src/parser/compare_commands.js
@@ -5,7 +5,7 @@ function Command_Compare_Memory(register_left, shift, base_register)
 	this.execute = function(state)
 	{
 		state.sign_flag = Math.sign(state.registers[this.register_left] - state.memory[this.address]);
-		state.source = state.registers[this.address];
+		state.source = state.memory[this.address];
 		state.tar = state.registers[this.register_left];
 		state.result = state.sign_flag;
 		state.state_changed = true;
